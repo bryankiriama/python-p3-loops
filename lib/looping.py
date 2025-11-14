51cfbe84b5df634686fa7c7c3eb58d5cfab560a6
bryankiriama <bryankiriama@gmail.com>
2025-11-14 19:45:58 +0300

    Used generator and list comprehensions to accomplish complex tasks quickly

--- lib/looping.py
+++ lib/looping.py
@@ -9,9 +9,17 @@ def happy_new_year():
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [num * num for num in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
